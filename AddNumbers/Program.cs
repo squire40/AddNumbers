7c4db586ba29aa08b3cb6b3bff9253c33c419ea5
Bourke, David <davidbourke@quickenloans.com>
2019-01-24 17:00:08 -0500

    AddNumbers complete

--- AddNumbers/Program.cs
+++ AddNumbers/Program.cs
@@ -10,6 +10,44 @@ namespace AddNumbers
     {
         static void Main(string[] args)
         {
+            bool shouldContinue;
+            int firstInt, secondInt;
+
+            do
+            {
+                shouldContinue = false;
+                Console.WriteLine("Enter first integer");
+                firstInt = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second integer");
+                secondInt = int.Parse(Console.ReadLine());
+
+                var result = AddNumbers(firstInt, secondInt);
+                Console.WriteLine();
+                Console.WriteLine($"Result is {result}");
+                Console.WriteLine();
+                Console.WriteLine("Continue? y/n");
+                var input = Console.ReadLine();
+                if (input.Equals("y", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    shouldContinue = true;
+                }
+            } while (shouldContinue);
         }
+
+        static int AddNumbers(int a, int b)
+        {
+            int result = b;
+
+            for (int i = 0; i < a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result += a;
+                }
+            }
+
+            return result;
+        }
+
     }
 }
